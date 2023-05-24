71f8ed69db5ab99a1e48b45a8f6cd54f89e45516
Denis Sazonov <greenfish@list.ru>
2023-05-24 08:49:49 +0400

    add models Profile, Review and migrations

---
+++ app/Models/Profile.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Profile extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['name', 'surname', 'image_url', 'about_me', 'city', 'date_of_birthday', 'course_id'];
+
+}
---
+++ database/migrations/2023_05_24_044500_create_reviews_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+
+            $table->text('text');
+            $table->unsignedBigInteger('profile_id');
+
+            $table->foreign('profile_id')
+                ->references('id')
+                ->on('profiles')
+                ->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reviews');
+    }
+};
