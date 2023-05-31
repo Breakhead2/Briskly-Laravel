4e8530cd577f69102667a4154f66cbe48bbb366e
Denis Sazonov <greenfish@list.ru>
2023-05-31 20:36:10 +0400

    inlude react app

---
+++ resources/views/react.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width,initial-scale=1" />
+    <title>Briskly School</title>
+    <link href="/img/favicon.ico" rel="icon" />
+    <script defer="defer" src="/static/js/main.04e9b7c4.js"></script>
+    <link href="/static/css/main.1c999b99.css" rel="stylesheet" />
+</head>
+<body>
+<div id="root"></div>
+</body>
+</html>
+
