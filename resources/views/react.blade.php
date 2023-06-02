1e6407ffa15e89d67ca837f15abb8cbf7c921d56
Denis Sazonov <greenfish@list.ru>
2023-06-02 22:49:02 +0400

    update react app

--- resources/views/react.blade.php
+++ resources/views/react.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width,initial-scale=1" />
     <title>Briskly School</title>
     <link href="/img/favicon.ico" rel="icon" />
-    <script defer="defer" src="/static/js/main.04e9b7c4.js"></script>
+    <script defer="defer" src="/static/js/main.e0e801fb.js"></script>
     <link href="/static/css/main.1c999b99.css" rel="stylesheet" />
 </head>
 <body>
