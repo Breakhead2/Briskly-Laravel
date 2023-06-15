00956984d3b90868f2e0f827b47f5f5c7c1d2257
Denis Sazonov <81635087+Breakhead2@users.noreply.github.com>
2023-06-15 12:28:46 +0400

    hidden props for Profile model

--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -10,5 +10,6 @@ class Profile extends Model
     use HasFactory;
 
     protected $fillable = ['name', 'surname', 'image_url', 'about_me', 'city', 'date_of_birthday', 'course_id'];
+    protected $hidden = ['created_at', 'updated_at'];
 
 }
