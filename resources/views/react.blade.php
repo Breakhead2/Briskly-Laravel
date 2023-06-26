eebae8ed16eca920ba1fdc4dbe0da367fadd95d4
Denis Sazonov <greenfish@list.ru>
2023-06-26 08:59:05 +0400

    add articles and vocabulary

--- resources/views/react.blade.php
+++ resources/views/react.blade.php
@@ -5,8 +5,8 @@
     <meta name="viewport" content="width=device-width,initial-scale=1" />
     <title>Briskly School</title>
     <link href="./img/favicon.ico" rel="icon" />
-    <script defer="defer" src="/static/js/main.af17fb2a.js"></script>
-    <link href="/static/css/main.d4d73144.css" rel="stylesheet" />
+    <script defer="defer" src="/static/js/main.3e5ae9d9.js"></script>
+    <link href="/static/css/main.7b4eab81.css" rel="stylesheet" />
 </head>
 <body>
 <div id="root"></div>
