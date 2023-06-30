a183d199dd523927fcdd2d79b817335cd120f1e3
Denis Sazonov <greenfish@list.ru>
2023-06-30 23:15:48 +0400

    fix bug with footer

--- resources/views/react.blade.php
+++ resources/views/react.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width,initial-scale=1" />
     <title>Briskly School</title>
     <link href="./img/favicon.ico" rel="icon" />
-    <script defer="defer" src="/static/js/main.404ce724.js"></script>
+    <script defer="defer" src="/static/js/main.ab8d0a93.js"></script>
     <link href="/static/css/main.0baee32c.css" rel="stylesheet" />
 </head>
 <body>
