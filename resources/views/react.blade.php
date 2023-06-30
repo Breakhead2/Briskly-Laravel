35c3656b1dac7bf20aac0a115666e26117e0dffd
Denis Sazonov <greenfish@list.ru>
2023-06-30 17:27:13 +0400

    update react form

--- resources/views/react.blade.php
+++ resources/views/react.blade.php
@@ -5,8 +5,8 @@
     <meta name="viewport" content="width=device-width,initial-scale=1" />
     <title>Briskly School</title>
     <link href="./img/favicon.ico" rel="icon" />
-    <script defer="defer" src="/static/js/main.f79e30ef.js"></script>
-    <link href="/static/css/main.c00f7028.css" rel="stylesheet" />
+    <script defer="defer" src="/static/js/main.404ce724.js"></script>
+    <link href="/static/css/main.0baee32c.css" rel="stylesheet" />
 </head>
 <body>
 <div id="root"></div>
