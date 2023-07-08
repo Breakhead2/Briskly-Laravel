040d074a7aeae9ebf843d745dc035676358f5294
Denis Sazonov <greenfish@list.ru>
2023-07-08 18:09:09 +0400

    fix bug with buy avatar

--- resources/views/react.blade.php
+++ resources/views/react.blade.php
@@ -5,8 +5,8 @@
     <meta name="viewport" content="width=device-width,initial-scale=1" />
     <title>Briskly School</title>
     <link href="./img/favicon.ico" rel="icon" />
-    <script defer="defer" src="/static/js/main.6d64d78d.js"></script>
-    <link href="/static/css/main.40757143.css" rel="stylesheet" />
+    <script defer="defer" src="/static/js/main.f1ca081e.js"></script>
+    <link href="/static/css/main.8e52e406.css" rel="stylesheet" />
 </head>
 <body>
 <div id="root"></div>
