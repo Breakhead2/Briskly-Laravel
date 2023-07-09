2e91373dc5ed4a89109adbe268c84fbd04475796
Denis Sazonov <greenfish@list.ru>
2023-07-09 21:37:32 +0400

    update react form

--- resources/views/react.blade.php
+++ resources/views/react.blade.php
@@ -5,7 +5,8 @@
     <meta name="viewport" content="width=device-width,initial-scale=1" />
     <title>Briskly School</title>
     <link href="./img/favicon.ico" rel="icon" />
-    <script defer="defer" src="/static/js/main.f1ca081e.js"></script>
+    <script src="//code.jivo.ru/widget/T1uaMffeaS" async></script>
+    <script defer="defer" src="/static/js/main.b1c2390a.js"></script>
     <link href="/static/css/main.8e52e406.css" rel="stylesheet" />
 </head>
 <body>
